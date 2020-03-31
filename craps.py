a986f002fcb73337a38e48e3746795c5cdb8ac69
vitorfgr <62887659+vitorfgr@users.noreply.github.com>
2020-03-31 19:25:55 -0300

    Update craps.py
    nesse inicio nos criei uma lista para ir armazenando as apostas, criei um comando para que o jogador pudesse sair da jogo toda vez que iniciava uma nova rodada e inicie as perguntas para saber aonde o jogador quer apostar

--- craps.py
+++ craps.py
@@ -1,2 +1,24 @@
 import random
-tipos = []
+saldo = 500
+game = True
+while game:
+    aposta =True
+    tipos = []
+    P = 0
+    F = 0
+    A = 0
+    T = 0
+    while aposta:
+        print('Seu saldo atual: {0}'.format(saldo))
+        print('Você está na rodada "Come out" do jogo! ')
+        #tipos possiveis de apostas:
+        s = input('Caso queria sair do jogo, digite "sair", caso contrario digite "ficar" ?')
+        if s == "sair":
+            aposta = False
+            game = False
+            print('Até mais!')
+        a = input('Você gostaria de aposta: Pass Line (P), Field (F), Any Craps (A) ou Twelve  (T), por favor escolha um de cada vez!')
+        if a == 'P':
+            tipos.append(a)
+            bet = int(input('Quanto quer apostar em Pass Line?'))
+
