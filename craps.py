7668dd12c68db38585a88e8cc8baaae7edad0fe8
Matheus Kwon <kwonmat@outlook.com.br>
2020-03-31 23:24:37 -0300

    Entradas válidas estão sendo inválidas

--- craps.py
+++ craps.py
@@ -4,21 +4,21 @@ game = True
 while game:
     aposta = True
     point = False
-#guarda os tipos de apostas:
+    #guarda os tipos de apostas:
     tipos = []
-#variáveis que guardam as apostas de cada tipo:
+    #variáveis que guardam as apostas de cada tipo:
     p = 0
     f = 0
     c = 0
     t = 0
     while aposta:
-#variáveis que identificam se a aposta já foi feita para que, então, não se guarde repetidos na lista de tipos
+        #variáveis que identificam se a aposta já foi feita para que, então, não se guarde repetidos na lista de tipos:
         i = 0
         l = 0
         z = 0
         print('Seu saldo atual: {0}'.format(saldo))
         print('Você está na rodada "Come out" do jogo!')
-#tipos possíveis de apostas:
+        #tipos possíveis de apostas:
         a = input('Você gostaria de apostar em: Pass Line ("p"), Field ("f"), Craps ("c") ou Twelve ("t")? Por favor escolha um de cada vez!\n'
         '(Caso queria sair do jogo, digite "sair")\n')
         while a != 'p' or 'f' or 'c' or 't' or 'sair':
@@ -27,7 +27,7 @@ while game:
             aposta = False
             game = False
             print('Até mais!')
-#condicionamentos de cada escolha de aposta:
+        #condicionamentos de cada escolha de aposta:
         elif a == 'p':
             while i < l:
                 if tipos[i] != 'p':
@@ -101,7 +101,7 @@ while game:
             if fim == 'd':
                 aposta = False
         l = len(tipos)
-#jogando os dados e verificando se ganhou ou não em cada aposta
+    #jogando os dados e verificando se ganhou ou não em cada aposta:
     if game:
         x = 0
         n = len(tipos)
@@ -123,7 +123,7 @@ while game:
                     lucro -= p
                 else:
                     point = True
-#variável para memorizar o point
+                    #variável para memorizar o point:
                     d = dados
                 x += 1
             if tipos[x] == 'f':
@@ -161,14 +161,14 @@ while game:
                     print('Perdeu em Twelve!')
                     lucro -= t
                 x += 1
-#diferença de dinheiro da rodada
+        #diferença de dinheiro da rodada
         if lucro < 0:
             print('Seu prejuízo é de {0}.'.format(-lucro))
         elif lucro == 0:
             print('Seu saldo continua igual.')
         else:
             print('seu lucro é de {0}.'.format(lucro))
-#jogo em point:
+    #jogo em point:
     while point:
         aposta2 = True
         tipos2 = ['p']
@@ -181,7 +181,7 @@ while game:
             z = 0
             print('Seu saldo atual: {0}'.format(saldo))
             print('Você está na rodada "Point" do jogo!')
-#tipos possíveis de apostas:
+            #tipos possíveis de apostas:
             a = input('Você gostaria de apostar em: Point ("p"), Field ("f"), Craps ("c") ou Twelve ("t")? Por favor escolha um de cada vez!\n'
             '(Caso queria sair do jogo, digite "sair")\n')
             while a != 'p' or 'f' or 'c' or 't' or 'sair':
@@ -191,7 +191,7 @@ while game:
                 point = False
                 game = False
                 print('Até mais!')
-#condicionamentos de cada escolha de aposta:
+            #condicionamentos de cada escolha de aposta:
             elif a == 'p':
                 bet = int(input('Quanto quer apostar em Point? (1:1)\n'))
                 while bet > saldo or bet < 0:
@@ -259,7 +259,7 @@ while game:
                 if fim == 'd':
                     aposta2 = False
             l = len(tipos)
-#jogando os dados e verificando se ganhou ou não em cada aposta
+        #jogando os dados e verificando se ganhou ou não em cada aposta
         if game:
             x = 0
             n = len(tipos)
@@ -317,7 +317,7 @@ while game:
                         print('Perdeu em Twelve!')
                         lucro -= t
                     x += 1
-#diferença de dinheiro da rodada
+            #diferença de dinheiro da rodada
             if lucro < 0:
                 print('Seu prejuízo é de {0}.'.format(-lucro))
             elif lucro == 0:
