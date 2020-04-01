513a4985eb609b978e02c6b14a92f3d16293c2ba
Matheus Kwon <kwonmat@outlook.com.br>
2020-03-31 21:12:17 -0300

    Update craps.py

--- craps.py
+++ craps.py
@@ -12,11 +12,13 @@ while game:
         print('Seu saldo atual: {0}'.format(saldo))
         print('Você está na rodada "Come out" do jogo! ')
         #tipos possiveis de apostas:
-        s = input('Caso queria sair do jogo, digite "sair", caso contrário digite "ficar" ?')
-        if s == "sair":
+        a = input('Você gostaria de apostar: Pass Line (p), Field (f), Any Craps (a) ou Twelve (t)? Por favor escolha um de cada vez!\n(Caso queria sair do jogo, digite "sair")')
+        while a != 'p' or 'f' or 'a' or 't' or 'sair':
+            a = input('Inválido! Digite alguma das opções: "p", "f", "a", "t" ou "sair"')
+        if a == "sair":
             aposta = False
             game = False
             print('Até mais!')
-        elif a == 'P':
+        elif a == 'p':
             tipos.append(a)
             bet = int(input('Quanto quer apostar em Pass Line?'))
